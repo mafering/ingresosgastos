2d4e85d409f6c4e80b92513c73ba4afa12f83f47
durdy48 <durdy48@7d5a3544-5123-11de-a74f-b79b8f70a355>
2009-06-17 12:51:25 +0000

    Aplicacion Spring-Hibernate-JSF-ICEFACES sin nada pero con el acceso a datos configurado y unos primeros fuentes pendientes de prueba

---
+++ IngresosGastos/src/main/java/es/antonio/duarte/IngresosGastos/bean/LoginBean.java
@@ -0,0 +1,85 @@
+package es.antonio.duarte.IngresosGastos.bean;
+
+
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+//import es.gdtel.afirma.Autenticacion;
+
+
+
+public class LoginBean{
+
+	private String usuario;
+	private String clave;
+	private String nombreCompleto;
+	//private Autenticacion autenticacion;
+	
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	public String login() {
+		if ("prueba".equals(usuario)
+				&& "prueba".equals(clave)) {
+			nombreCompleto = "Prueba Prueba";
+			return "ok";
+		} else {
+			return "error";
+		}
+	}
+
+	
+	public String logout() {
+		usuario = "";
+		clave = "";
+		nombreCompleto = "";
+		//autenticacion = null;
+		((HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true)).invalidate();
+		return "logout";
+	}
+	
+	public String getUrlautenticacion () {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+		//return Autenticacion.getURLAutenticacion(session.getId());
+		return "ok";
+	}
+
+	public boolean getAutenticar () {
+		String sessionId = ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).getId();
+		String datos = (String) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("datos");
+		NavigationHandler navigationHandler = FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+		//autenticacion = new Autenticacion();
+		String resultado = "logout";
+		//if (autenticacion.autenticar(datos, sessionId)) {
+			//nombreCompleto = autenticacion.getNombreCompleto();
+			nombreCompleto = "Antonio Duarte";
+			resultado = "ok";
+		//}
+		if (!FacesContext.getCurrentInstance().getResponseComplete()) {
+			navigationHandler.handleNavigation(FacesContext.getCurrentInstance(), null, resultado);
+		}
+		return true;
+	}
+}
