d73416e25d6d9e002fc3e0b0e4cb9e138fe0188d
durdy48 <durdy48@7d5a3544-5123-11de-a74f-b79b8f70a355>
2009-07-07 12:47:26 +0000

    Dos nuevos metodos para buscar por tipo o por fecha ingresos o gastos

--- trunk/I-G/src/es/antonio/duarte/dao/IngresosGastosDAO.java
+++ trunk/I-G/src/es/antonio/duarte/dao/IngresosGastosDAO.java
@@ -1,5 +1,8 @@
 package es.antonio.duarte.dao;
 
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * DAO de IngresosGastos 
  * @author Antonio Duarte
@@ -8,4 +11,20 @@ package es.antonio.duarte.dao;
 
 public interface IngresosGastosDAO extends DAOGeneral{
 
+		/**
+	    * Consulta de ingresosgastos por fecha.
+	    * @param fecha Fecha por la cual se buscaran ingresosgastos
+	    * @return Lista de ingresosgastos que cumplen con la fecha (vacia si no hay
+	    *         registros)
+	    */
+	   public Collection consultarPorFecha(Date fecha);
+
+	   /**
+	    * Consulta de ingresosgastos por tipo.
+	    * @param tipo Tipo por el cual se buscaran ingresosgastos
+	    * @return Lista de ingresosgastos que cumplen con el tipo (vacia si no hay
+	    *         registros)
+	    */
+	   public Collection consultarPorTipo(String tipo);
+	   
 }
